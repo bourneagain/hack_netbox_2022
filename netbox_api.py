24a0436a52daa1811acdc35d354de6502f0b05ca
Shyam Rajendran <rshyam.psg@gmail.com>
2022-09-20 12:07:34 -0700

    Update netbox_api.py

--- netbox_api.py
+++ netbox_api.py
@@ -1,10 +1,21 @@
 import pynetbox
-nb = pynetbox.api("http://172.19.249.189:8000")
-token = nb.create_token("admin", "admin")
 from pprint import pprint
+
+# Connect to running netbox instance
+nb = pynetbox.api("http://172.19.249.189:8000")
+
+# Create token for auth
+token = nb.create_token("admin", "admin")
+
+
 pprint(dict(token))
 pprint(list(nb.dcim.devices.all()))
+
+
 nb = pynetbox.api("http://172.19.249.189:8000", token=token);
+
+# list available devices
 for device in devices:
     print(device.name)
 
+# Create a new device
