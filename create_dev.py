f30779a9d2b75f65f642dd5505cc006fe8468258
Shyam Rajendran <shrajendran@microsoft.com>
2022-09-20 12:35:48 -0700

    remote def

--- create_dev.py
+++ create_dev.py
@@ -1,7 +1,5 @@
 import pynetbox
-import time
 
-dev = "R3"
 token = "4ac8ed5e369eb445b02cfa422a3db26723c6ad8b"
 def adddev(dev):
     nb = pynetbox.api(url="http://172.19.249.189:8000", token=token)
